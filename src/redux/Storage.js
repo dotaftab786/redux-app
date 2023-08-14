bb8183d2a0aa87eadb78ec20cdc032bb06e16763
Aftab Alam <alam69745@gmail.com>
2023-08-14 23:29:59 +0530

    middleware basics demo

--- src/redux/Storage.js
+++ src/redux/Storage.js
@@ -1,9 +1,20 @@
 import {
-  createStore
+  createStore,
+  applyMiddleware
 } from "redux";
 
 import UserReducer from "./reducer/User.reducer";
 
-const Storage = createStore(UserReducer);
+const report = (Storage)=>(next)=>(action)=>{
+  console.log("First middleware code executed");
+  next(action);
+}
+
+const demo = (Storage)=>(next)=>(action)=>{
+  console.log("Second middleware code executed");
+  next(action)
+}
+
+const Storage = createStore(UserReducer,{},applyMiddleware(report,demo));
 
 export default Storage;
